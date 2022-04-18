b304341535dc12cc94e35838086b25ea0bf2fdfa
Fernando <osocaramelosofer@hotmail.com>
2022-04-17 23:26:11 -0500

    Changed structure pf main page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Sidebar from "./components/Sidebar.vue";
-import Maincontent from "./components/MainContent.vue";
 </script>
 
 <template>
-  <div class="bg-nora-gray-200 grid grid-cols-main  py-20 gap-x-14 px-10 xl:px-48 ">
-    <Sidebar />
-    <Maincontent />
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
