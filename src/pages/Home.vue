5afc7f452c7a4d2ec8699c3f5415165d72f917da
Fernando <osocaramelosofer@hotmail.com>
2022-04-17 23:25:46 -0500

    Create pages

---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Sidebar from "../components/Sidebar.vue";
+import Maincontent from "../components/MainContent.vue";
+</script>
+
+<template>
+  <div
+    class="
+      bg-nora-gray-200
+      grid
+      laptop:grid-cols-main
+      py-20
+      gap-x-14
+      px-10
+      xl:px-48
+      grid-cols-1
+    "
+  >
+    <Sidebar />
+    <Maincontent />
+  </div>
+</template>
