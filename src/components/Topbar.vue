7a82db66effca8822f1326b18a2f06839f292c1c
Fernando <osocaramelosofer@hotmail.com>
2022-04-19 16:22:15 -0500

    Fixed responsive design

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,23 +3,12 @@ import Dropdown from './Dropdown.vue'
 </script>
 
 <template>
-  <div
-    class="
-      bg-nora-blue-300
-      flex
-      items-center
-      justify-between
-      py-2
-      px-5
-      rounded-lg
-      mb-5
-    "
-  >
+  <div class="bg-nora-blue-300 flex items-center justify-between py-2 px-5 tablet:rounded-lg mb-5">
     <div class="flex">
-      <div class="w-10 h-10 mr-3">
+      <div class="w-10 h-10 mr-3 hidden tablet:block">
         <img src="../assets/idea.png" class="h-full w-full object-fill" />
       </div>
-      <span class="text-white font-bold text-lg mr-5">6 Suggestions</span>
+      <span class="text-white font-bold text-lg mr-5 hidden tablet:block">6 Suggestions</span>
       <Dropdown />
 
     </div>
