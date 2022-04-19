fb9a7a8abf4eef2914dfa44fbe3f6dbb40c195d2
Fernando <osocaramelosofer@hotmail.com>
2022-04-19 16:22:00 -0500

    Added sidebar for mobile

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {ref} from 'vue'
+
 import Boardsuggestions from "./BoardSuggestions.vue";
 import Boardroadmap from "./BoardRoadmap.vue";
 
@@ -21,20 +23,50 @@ const roadmapObjects = [
     number: 1,
   },
 ];
+
+const showMenu = ref(false)
+
+const toggleMenu = () => showMenu.value = !showMenu.value
+
+
 </script>
 
 <template>
-  <aside class="laptop:w-64 laptop:gap-y-5 laptop:grid-cols-none laptop:content-start grid  grid-cols-3 gap-x-5 mb-12">
-    <div class="frontend-mentor pl-5 pr-10 pb-6 rounded-md h-36 flex items-end">
-      <div>
-        <span class="block font-bold text-xl text-white">Frontend Mentor</span>
-        <span class="block font-medium text-nora-gray-300 text-sm">Feedback Board</span>
+  <div class="hidden tablet:block">
+      <aside class="laptop:w-64 laptop:gap-y-5 laptop:grid-cols-none laptop:content-start grid  grid-cols-3 gap-x-5 mb-12">
+      <div class="frontend-mentor laptop:pt-16 pl-5 pr-10 pb-6 rounded-md flex items-end">
+        <div>
+          <span class="block font-bold text-xl text-white">Frontend Mentor</span>
+          <span class="block font-medium text-nora-gray-300 text-sm">Feedback Board</span>
+        </div>
+      </div>
+
+      <Boardsuggestions :suggestions="suggestions" />
+      <Boardroadmap :roadmapObjects="roadmapObjects"/>
+    </aside>
+  </div>
+
+  <div class="relative tablet:hidden">
+    <nav class="bg-mobile px-2 sm:px-4 py-2.5 flex justify-between">
+      <div class="flex flex-col">
+        <span class="font-bold text-2xl text-white">Frontend Mentor</span>
+        <span class="text-2xl text-white">Feedback Board</span>
+      </div>
+
+      <div class="relative">
+        <span @click="toggleMenu" v-if="showMenu" class="font-medium text-5xl text-white cursor-pointer">x</span>
+        <span @click="toggleMenu" v-else class="font-medium text-5xl text-white cursor-pointer"> = </span>
+      </div>
+    </nav>
+    <div v-if="showMenu" class="blur-bg w-screen h-screen absolute z-10">
+      <div class="top absolute bg-nora-gray-100 pl-10 pr-14 pt-10  right-0  h-screen w-3/5 flex flex-col gap-y-10">
+        <Boardsuggestions :suggestions="suggestions" />
+        <Boardroadmap :roadmapObjects="roadmapObjects"/>
       </div>
     </div>
+  </div>
 
-    <Boardsuggestions :suggestions="suggestions" />
-    <Boardroadmap :roadmapObjects="roadmapObjects"/>
-  </aside>
+
 </template>
 
 <style>
@@ -45,5 +77,12 @@ const roadmapObjects = [
     rgba(63, 94, 251, 1) 0%,
     rgba(252, 70, 107, 1) 100%
   );
+}
+
+.bg-mobile {
+  background-image: linear-gradient(to right, #7ad8fb, #7bc5ff, #99adfd, #bf91e6, #dd71bc, #ec69a2, #f36686, #f2696a, #f67d69, #f9906b, #faa371, #fbb57a);
+}
+.blur-bg {
+  background-color: rgba(7, 7, 7, .65);
 }
 </style>
