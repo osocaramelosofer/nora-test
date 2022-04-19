8423edf2f2ce2d13cbd3bc05aff05fcd43d562c1
Fernando <osocaramelosofer@hotmail.com>
2022-04-19 16:18:42 -0500

    Created suggestion tag component

---
+++ src/components/SuggestionTag.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>
+<template>
+  <div
+    class="
+      bg-nora-gray-300
+      rounded-md
+      px-3
+      py-1
+      flex
+      items-center
+      justify-center
+      cursor-pointer
+    "
+  >
+    <span class="text-nora-blue-200 font-semibold text-sm">{{ text }}</span>
+  </div>
+</template>
