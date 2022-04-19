ef0022b5b6db54f52ad94f38ee1ab6cfa89e6931
Fernando <osocaramelosofer@hotmail.com>
2022-04-19 16:19:08 -0500

    Added suggestion tag component

--- src/components/BoardSuggestions.vue
+++ src/components/BoardSuggestions.vue
@@ -1,11 +1,12 @@
 <script setup>
+import SuggestionTag from './SuggestionTag.vue'
 const props = defineProps({
   suggestions: Array,
 });
 </script>
 
 <template>
-  <div class="py-4 pl-5 pr-10 rounded-md h-36 bg-white flex flex-wrap gap-2">
+  <!-- <div class="py-4 pl-5 pr-10 rounded-md h-36 bg-blue-300 flex flex-wrap gap-2">
     <div
       v-for="(suggestion, index) in suggestions"
       :key="index"
@@ -14,6 +15,24 @@ const props = defineProps({
       <span class="text-nora-blue-200 font-semibold text-xs">{{
         suggestion
       }}</span>
+    </div>
+  </div> -->
+  <div class="rounded-md bg-white grid gap-y-3 pl-4 pt-5 pr-3 pb-8 laptop:pr-14">
+    <div class="flex gap-x-2">
+      <SuggestionTag text="All" />
+      <SuggestionTag text="UI" />
+      <SuggestionTag text="UX" />
+    </div>
+
+    <div class="flex gap-x-2">
+      <div class="bg-nora-gray-300 rounded-md px-3 flex items-center justify-center cursor-pointer grow">
+        <span class="text-nora-blue-200 font-semibold text-sm">Enhancement</span>
+      </div>
+      <SuggestionTag text="Bug" />
+    </div>
+
+    <div class="flex justify-self-start gap-x-4">
+      <SuggestionTag text="Feature" />
     </div>
   </div>
 </template>
