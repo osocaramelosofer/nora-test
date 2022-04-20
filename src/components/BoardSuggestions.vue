2b5fe2ea38b889df27ef9c2a9e1dc22fa686f731
Fernando <osocaramelosofer@hotmail.com>
2022-04-20 12:12:22 -0500

    Removed comments

--- src/components/BoardSuggestions.vue
+++ src/components/BoardSuggestions.vue
@@ -6,17 +6,6 @@ const props = defineProps({
 </script>
 
 <template>
-  <!-- <div class="py-4 pl-5 pr-10 rounded-md h-36 bg-blue-300 flex flex-wrap gap-2">
-    <div
-      v-for="(suggestion, index) in suggestions"
-      :key="index"
-      class="bg-nora-gray-300 rounded-xl px-3 flex items-center cursor-pointer"
-    >
-      <span class="text-nora-blue-200 font-semibold text-xs">{{
-        suggestion
-      }}</span>
-    </div>
-  </div> -->
   <div class="rounded-md bg-white grid gap-y-3 pl-4 pt-5 pr-3 pb-8 laptop:pr-14">
     <div class="flex gap-x-2">
       <SuggestionTag text="All" />
