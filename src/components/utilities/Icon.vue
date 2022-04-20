40b8aea9e22b55313e0505951abccb44e8305082
Fernando <osocaramelosofer@hotmail.com>
2022-04-20 12:12:34 -0500

    Removed comments

--- src/components/utilities/Icon.vue
+++ src/components/utilities/Icon.vue
@@ -10,7 +10,5 @@ const props = defineProps({
 </script>
 <template>
   <div :style="{ width: width + 'px', height: height + 'px' }">
-    <!-- <img src="../../assets/up-arrow.svg" :alt="alt" class="w-full h-full object-fill" /> -->
-    <!-- <img :src="require('@/assets/' + filename)" :alt="alt" class="w-full h-full object-fill" /> -->
   </div>
 </template>
